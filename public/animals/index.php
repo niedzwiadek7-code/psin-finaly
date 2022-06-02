33d2e1a8fafdd77478f30cf1eac8fb0b14204a06
Damian Kliber <damiankliber@gmail.com>
2022-06-02 21:36:25 +0200

    fix: standard fixing

--- public/animals/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-    require_once "../../src/utils/Dependency.php";
-    require_once ROOTPATH . "/framework/Page/TableTemplate.php";
-
-    $table = new TableTemplate();
-    echo $table->build();
--- public/tables/generate.php
+++ public/tables/generate.php
@@ -1,1 +1,6 @@
 <?php
+    require_once "../../src/utils/Dependency.php";
+    require_once ROOTPATH . "/framework/Page/TableTemplate.php";
+
+    $table = new TableTemplate();
+    echo $table->build();
